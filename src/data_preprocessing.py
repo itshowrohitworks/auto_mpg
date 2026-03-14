3f02734f824de671276985c40edfb4f2585962c9
itshowrohitworks <therohitshrivastava@gmail.com>
2026-03-14 18:54:51 +0530

    Wrote the Data preprocessing function.

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,7 +4,22 @@ from data.auto_mpg import get_data
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = get_data()
+def preprocessing(data):
 
-print(df.head())
+    # Dropping unwanted columns:
+    data.drop(df.select_dtypes('str'),axis=1,inplace=True)
+
+    # Handling Missing Values:
+    impute = SimpleImputer(strategy="mean")
+    missing_value_colm = df.columns[df.isnull().any()].tolist()
+    df[missing_value_colm] = impute.fit_transform(df[missing_value_colm])
+
+    # Input and Output features:
+    X = df.drop('mpg',axis=1)
+    y = df['mpg']
+
+    return X,y
