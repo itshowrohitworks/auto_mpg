98854faf6c8e29fe9ffa0c3e6b86fd1b7b7a7550
itshowrohitworks <therohitshrivastava@gmail.com>
2026-03-14 17:19:06 +0530

    Changed psycopg3 engine to  SQL Alchemy

--- data/db.py
+++ data/db.py
@@ -1,15 +1,21 @@
 import psycopg
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 load_dotenv()
 
-def get_connection():
-    conn = psycopg.connect(
-        host = os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+def get_engine():
+    host = os.getenv("DB_HOST")
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")    
+
+    DATABASE_URL = (
+        f"postgresql+psycopg://{user}:{password}"
+        f"@{host}:{port}/{dbname}"
     )
-    return conn
+    engine = create_engine(DATABASE_URL)
+
+    return engine
